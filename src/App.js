5bd629c72f128191ef515ccc9cb1bb8eb1016573
MathiasDininno <dininnom1@gmail.com>
2023-02-05 15:36:16 -0300

    Ultimo paso en mi app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/contador';
+import ReactLogo from './imagenes/React_logo_wordmark.png'
+import { useState } from 'react';
+
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0)
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1)
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logoDeMiProyecto'>
+        <img
+          className='logo'
+          src={ReactLogo}
+          alt='Logo de React'
+        />
+      </div>
+      <div className='contenedor-principal'>
+      <Contador numClics={numClics} />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClick={manejarClic} />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClick={reiniciarContador} />
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Boton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../hojas-de-estilos/boton.css';
+
+function Boton({ texto, esBotonDeClic, manejarClick }) {
+  return (
+    <button className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'} onClick={manejarClick}>
+      {texto}
+    </button>
+  )
+}
+
+export default Boton
